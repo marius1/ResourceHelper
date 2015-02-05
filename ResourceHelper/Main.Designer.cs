fb88880b2458f07b1e8e493f08ddc6b1fa34c14a
Marius Bronstring <marius@funda.nl>
2015-02-05 10:09:28 +0100

    Initial

---
+++ ResourceHelper/Main.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ResourceHelper
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstKeys = new System.Windows.Forms.ListBox();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstKeys
+            // 
+            this.lstKeys.FormattingEnabled = true;
+            this.lstKeys.Location = new System.Drawing.Point(12, 38);
+            this.lstKeys.Name = "lstKeys";
+            this.lstKeys.Size = new System.Drawing.Size(243, 290);
+            this.lstKeys.TabIndex = 1;
+            this.lstKeys.SelectedIndexChanged += new System.EventHandler(this.lstKeys_SelectedIndexChanged);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(12, 12);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(243, 20);
+            this.txtFilter.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 347);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(243, 106);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Output";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 465);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.lstKeys);
+            this.Name = "Main";
+            this.Text = "Resource helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstKeys;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ResourceHelper/Main.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ResourceHelper
+{
+    public partial class Main : Form
+    {
+        private Dictionary<string, TranslationNode> keys = new Dictionary<string, TranslationNode>();
+        private List<string> show = new List<string>();
+
+        public Main()
+        {
+            InitializeComponent();
+
+            txtFilter.TextChanged += txtFilter_TextChanged;
+
+            var resourceFiles = new List<string>()
+            {
+                @"C:\Repos\central\4_PresentatieLaag\Controls\Translation\GlobalResources.en.resx",
+                @"C:\Repos\central\4_PresentatieLaag\Controls\Translation\GlobalResources.resx",
+                @"C:\Repos\central\4_PresentatieLaag\Funda.Web.Common.Resources\Strings.en.resx",
+                @"C:\Repos\central\4_PresentatieLaag\Funda.Web.Common.Resources\Strings.resx"
+            };
+            
+            foreach (var file in resourceFiles)
+            {
+                var doc = new XmlDocument();
+                doc.Load(file);
+
+                foreach (XmlNode node in doc.SelectNodes("//root/data"))
+                {
+                    if (node.Attributes == null || node.Attributes["name"] == null) 
+                        continue;
+
+                    string name = node.Attributes["name"].InnerText;
+                    var value = node.SelectSingleNode("value");
+                    if (value == null)
+                        continue;
+
+                    if (keys.ContainsKey(name))
+                    {
+                        keys[name].Translations.Add(value.InnerText);
+                    }
+                    else
+                    {
+                        keys.Add(name, new TranslationNode()
+                        {
+                            Key = name, 
+                            Translations = new List<string>()
+                            {
+                                value.InnerText
+                            }
+                        });
+                    }
+                }
+            }
+
+            show.AddRange(keys.Keys);
+
+            lstKeys.Items.AddRange(show.ToArray());
+        }
+
+        private void txtFilter_TextChanged(object sender, EventArgs e)
+        {
+            show.Clear();
+
+            foreach (var key in keys)
+            {
+                if (key.Key.Contains(txtFilter.Text))
+                    show.Add(key.Key);
+            }
+
+            lstKeys.Items.Clear();
+            lstKeys.Items.AddRange(show.ToArray());
+
+            if (show.Count > 0)
+            {
+                lstKeys.SelectedIndex = 0;
+            }
+        }
+
+        private void lstKeys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lstKeys.SelectedIndex;
+            if (index >= 0)
+            {
+                var key = lstKeys.Items[index].ToString();
+                if (keys.ContainsKey(key))
+                {
+                    var output = "";
+                    foreach (var translation in keys[key].Translations)
+                    {
+                        output += translation + Environment.NewLine;
+                    }
+                    txtOutput.Text = output;
+                }
+            }
+        }
+    }
+
+    public class TranslationNode
+    {
+        public List<string> Translations { get; set; }
+        public string Key { get; set; }
+    }
+}
